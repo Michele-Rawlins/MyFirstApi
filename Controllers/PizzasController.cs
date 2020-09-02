d1c7923bf05b5cf7d78ab60776e94a0cd73677ff
Michele Rawlins <shellbop@att.net>
2020-09-02 12:40:46 -0500

    Add project files.

---
+++ Controllers/PizzasController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstApi.Models;
+
+namespace MyFirstApi.Controllers
+{
+    [Route("api/pizzas")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        static List<Pizza> _pizzas;
+       static PizzasController()
+        {
+            var pizza = new Pizza { Id = 1, Size = "Medium", Toppings = new List<string> { "Pepperoni" } };
+            var pizza2 = new Pizza { Id = 2, Size = "Medium", Toppings = new List<string> { "Sausage" } };
+            var pizza3 = new Pizza { Id = 3, Size = "Medium", Toppings = new List<string> { "Bacon" } };
+            
+            _pizzas = new List<Pizza> { pizza, pizza2, pizza3 };
+        }
+
+        [HttpGet]
+        public List<Pizza> GetAllPizzas()
+        {
+            return _pizzas;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPizzaById(int id)
+        {
+            var result = _pizzas.SingleOrDefault(Pizza => Pizza.Id == id);
+
+            if(result == null)
+            {
+                return NotFound($"Could not find a pizza with the id {id}");
+            }
+            return Ok(result);
+
+        }
+        [HttpPost]
+        public IActionResult CreatePizza(CreatePizzaCommand command)
+        {
+            
+            var newPizza = new Pizza { Size = command.Size};
+            newPizza.Id = _pizzas.Select(p => p.Id).Max() + 1;
+            
+            _pizzas.Add(newPizza);
+
+            return Created("api/pizzas[newPizza.Id]", newPizza);
+
+                               }
+       
+
+    }
+}
